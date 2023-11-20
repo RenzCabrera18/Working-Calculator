563b06ab7cd81e1c0fed7637027fa0f7003397a9
Renz <148547547+RenzCabrera18@users.noreply.github.com>
2023-11-20 20:56:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+let currentInput = "";
+let currentOperator = "";
+
+function clearinput() {
+  currentInput = "";
+  currentOperator = "";
+  document.getElementById("result").value = "";
+}
+
+function appendNumber(number) {
+  currentInput += number;
+  document.getElementById("result").value = currentInput;
+}
+
+function add() {
+  currentOperator = "+";
+  currentInput += currentOperator;
+  document.getElementById("result").value = currentInput;
+}
+
+function subtract() {
+  currentOperator = "-";
+  currentInput += currentOperator;
+  document.getElementById("result").value = currentInput;
+}
+
+function multiply() {
+  currentOperator = "*";
+  currentInput += currentOperator;
+  document.getElementById("result").value = currentInput;
+}
+
+function divide() {
+  currentOperator = "/";
+  currentInput += currentOperator;
+  document.getElementById("result").value = currentInput;
+}
+
+function calculate() {
+  const result = eval(currentInput);
+  console.log(result);
+  document.getElementById("result").value = result;
+  currentInput.toString();
+  currentOperator = "";
+}
